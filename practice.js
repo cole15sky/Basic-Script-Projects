1f19d396689c3905dd620a20e90b213f9fc951ee
sky___ <sky___@Aakashs-MacBook-Pro.local>
2024-05-23 18:00:37 +0545

    CountDown Img Display

---
+++ practice.js
@@ -0,0 +1,17 @@
+console.log("check! js loaded");
+var countdownElement= document.getElementById('count-down');
+var backgroundPath =document.getElementById('bg-img');
+
+var initialCountdownVal = countdownElement.innerHTML;
+
+setInterval (function(){
+initialCountdownVal = initialCountdownVal >0? initialCountdownVal -1: 0;
+countdownElement.innerHTML=initialCountdownVal;
+ var backImgpath = initialCountdownVal %2 ===0? 'img/2.png' : 'img/1.png';
+ backgroundPath.src =backImgpath;
+
+
+},1000);
+
+
+
