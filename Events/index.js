19ce888cfd1f9a72c7efb239a0a3474a9874041f
sky___ <sky___@Aakashs-MacBook-Pro.local>
2024-05-28 12:31:22 +0545

    Handeling events via.event properties

--- Events/index.js
+++ Events/index.js
@@ -1,6 +1,12 @@
 
 var  btn = document.getElementById('click-button');
 
-btn.onclick =function(){
-    alert("I'm click");
+// we can handel the event properties by two ways.
+
+// through event properties as;
+
+function onBtnClick() {
+    alert("I'm clicked.");
 }
+
+btn.onclick = onBtnClick;
